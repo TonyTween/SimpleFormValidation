1a8ea71ad819454e872d1007657e3d82c12a9650
TonySemikin <tony.semikin@gmail.com>
2019-04-08 12:29:59 +0200

    Got the input fields

---
+++ script.js
@@ -0,0 +1,5 @@
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
+const email = document.querySelector('#email');
